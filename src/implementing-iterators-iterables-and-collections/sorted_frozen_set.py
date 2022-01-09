4bbaeed937989fc4bd4e1388a660f4d6d97db8e4
p-a <peter.augustinak@gmail.com>
2022-01-09 15:42:25 +0100

    Iterators and Iterables - Sequence Prtocol - implementing __hash__, reverse, index, count, __add__, __mul__ and __rmul__

--- src/implementing-iterators-iterables-and-collections/sorted_frozen_set.py
+++ src/implementing-iterators-iterables-and-collections/sorted_frozen_set.py
@@ -1,7 +1,11 @@
-class SortedFrozenSet:
+from collections.abc import Sequence
+from itertools import chain
+
+
+class SortedFrozenSet(Sequence):
 
    def __init__(self, items=None):
-      self._items = sorted(set(items)) if (items is not None) else set()
+      self._items = tuple(sorted(set(items)) if (items is not None) else set())
 
    def __contains__(self, item):
       return item in self._items
@@ -23,7 +27,11 @@ class SortedFrozenSet:
       return "{type}({arg})".format(
          type=type(self).__name__,
          arg=(
-            repr(self._items)
+            "[{}]".format(
+               ", ".join(
+                  map(repr, self._items)
+               )
+            )
             if self._items else ""
          )
       )
@@ -33,3 +41,19 @@ class SortedFrozenSet:
       if not isinstance(rhs, type(self)):
          return NotImplemented
       return self._items == rhs._items
+
+   def __hash__(self):
+      return hash(
+         (type(self), self._items)
+      )
+
+   def __add__(self, rhs):
+      if not isinstance(rhs, type(self)):
+         return NotImplemented
+      return SortedFrozenSet(chain(self._items, rhs._items))
+
+   def __mul__(self, rhs):
+      return self if rhs > 0 else SortedFrozenSet()
+
+   def __rmul__(self, lhs):
+      return self * lhs
